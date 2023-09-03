ff4378905fae8cf364ae9520f4458dfb9d9dc6a9
nkalupahana <avishkank@gmail.com>
2023-09-03 16:36:02 -0500

    Full chain working

--- generate_tests.py
+++ generate_tests.py
@@ -11,6 +11,9 @@ llm = OpenAI()
 prompt = PromptTemplate.from_template("Generate tests in Python (compatible with pytest) that produce 100% code coverage. Output only Python code and nothing else before or after. \n ```py\n{code}\n```")
 
 for function in itertools.chain(glob.glob("functions/*.py")):
+    if "__init__" in function:
+        continue
+
     # Get code and create prompt
     print(function)
     with open(function) as f:
@@ -20,7 +23,9 @@ for function in itertools.chain(glob.glob("functions/*.py")):
     # Generate tests and clean up output
     output = llm.predict(code_prompt)
     output = output.replace("```python", "").replace("```py", "").replace("```", "").strip()
+    filename = function.replace("functions/", "").replace(".py", "")
+    output = f"from functions.{filename} import {filename}\n" + output
 
     # Write out final code
-    with open(function.replace("functions/", "tests/"), "w") as f:
+    with open(f"tests/test_{filename}.py", "w") as f:
         f.write(output)
--- tests/test_divide.py
+++ tests/test_divide.py
@@ -3,11 +3,7 @@ import pytest
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
-        divide(10, 0)
+        divide(2, 0)
 
-def test_divide_integers():
-    assert divide(4,2) == 2
-    assert divide(17,3) == 5.666666666666667
-
-def test_divide_floats():
-    assert divide(3.5, 1.5) == 2.3333333333333335
+def test_divide_by_nonzero():
+    assert divide(2, 1) == 2
